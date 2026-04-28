e91799507baa1a640d2f1a91c90dc7e2784b0f77
FarManu-au <s.godpower@studenti.unina.it>
2026-04-28 19:11:36 +0200

    aggiornato utente

--- src/main/java/model/TestModel.java
+++ src/main/java/model/TestModel.java
@@ -3,9 +3,9 @@ package model;
 public class TestModel {
 
 	public static void main(String[] args) {
-		Utente u = new Utente("topolino","minni");
-		System.out.println(u.login("pippo","pluto"));
-		System.out.println(u.login("topolino","minni"));
+		Utente u = new Utente("topolino","minni","topolino@gmail.com","morfeo","macazo");
+		u.saluto();
+	
 
 	}
 
--- src/main/java/model/Utente.java
+++ src/main/java/model/Utente.java
@@ -3,15 +3,25 @@ package model;
 import java.util.ArrayList;
 
 public class Utente {
-    private String login;
-    private String password;
+    protected String nome;
+    protected string cognome;
+    protected String email;
+    protected String login;
+    protected String password;
 
-    public Utente(String login, String password) {
+    public Utente(String nome,String cognome, String email,String login, String password) {
+       this.nome=nome;
+       this.cognome=cognome;
+       this.email=email;
         this.login = login;
         this.password = password;
     }
 
     public boolean login(String login, String password) {
         return ( login.equals(this.login) && password.equals(this.password));
+    }
+
+    public void saluto(){
+  System.out.println("ciao mi chiamo" + this.nome+" "+this.cognome);
     }
 }
