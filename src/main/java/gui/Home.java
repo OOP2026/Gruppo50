66f8f07c0b5b3cfb1479a1308b88e0ed1c4377e4
FarManu-au <s.godpower@studenti.unina.it>
2026-05-27 18:45:17 +0200

    Definito la SchermataDocente, aggiunto un nuovo modo per mostrare sullo schermo l'errore in login o in registrazione

--- src/main/java/gui/Home.java
+++ src/main/java/gui/Home.java
@@ -7,7 +7,11 @@ import java.util.ArrayList;
 public class Home {
 	public static void main(String[] args) {
 		Controller controller = new Controller(new ArrayList<>());
-		PrimoFrame primoFrame = new PrimoFrame(controller);
-		primoFrame.frame.setVisible(true);
+		//PrimoFrame f = new PrimoFrame(controller);
+		JFrame frm= new JFrame();
+		SchermataDocente f= new SchermataDocente(controller,frm);
+		f.frame.setVisible(true);
+		
 	}
 }
+
