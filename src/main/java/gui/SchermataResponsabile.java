cab1a9a0319c64ab497004b7575fbf7649771ab7
danking64 <daniele.massa@studenti.unina.it>
2026-05-29 11:38:57 +0200

    piccole modifiche
    aggiunta homework 1

--- src/main/java/gui/SchermataResponsabile.java
+++ src/main/java/gui/SchermataResponsabile.java
@@ -6,7 +6,7 @@ import javax.swing.*;
 public class SchermataResponsabile {
     JFrame frame;
     private JPanel panel1;
-    private JButton VIsualizzaRichiesteButton;
+    private JButton VisualizzaRichiesteButton;
     private JButton creaLezioneButton;
     private JButton insegnamentiAttiviButton;
 
