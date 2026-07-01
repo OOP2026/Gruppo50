aec0b7ea32fcc7c4fe10507b7b3b07d9743aba7e
FarManu-au <s.godpower@studenti.unina.it>
2026-07-02 00:13:34 +0200

    Aggiunto AulaDAO/AulaPostgresDAO (WIP)
    Non ancora testato.

---
+++ src/main/java/implementazioneDao/AulaPostgresDao.java
@@ -0,0 +1,57 @@
+package implementazioneDao;
+
+
+import dao.AulaDAO;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class AulaPostgresDao implements AulaDAO {
+    private final Connection connection;
+
+    public AulaPostgresDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    @Override
+    public void salvaAulaDB(String nome, int capienza) throws Exception {
+        String sql = "INSERT INTO aula (nome,capienza) " +
+                "VALUES (?,?)";
+        try (PreparedStatement ps = connection.prepareStatement(sql)) {
+            ps.setString(1, nome);
+            ps.setInt(2, capienza);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new Exception("Impossibile salvare salvare l'aula nel DataBase: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Object[][] caricaAulaDB() {
+        String sql="SELECT * FROM aula";
+        List<Object[]> aule= new ArrayList<>();
+        boolean haRighe=false;
+        try(PreparedStatement ps=connection.prepareStatement(sql)){
+
+            ResultSet rs= ps.executeQuery();
+
+          while(rs.next()){
+              haRighe=true;
+              String nome= rs.getString("Aula");
+              int capienza= rs.getInt("capienza");
+              aule.add(new Object[]{nome,capienza});
+          }
+rs.close();
+        }catch (SQLException e){
+            System.out.println("Errore nel prendere le aule dal database: "+e.getMessage());
+        }
+if(!haRighe){
+    return new Object[][]{{"Nessuna aula presente",0}};
+}
+        return aule.toArray(new Object[0][]);
+    }
+}
