6e757673c7287c571d7755cf54a0ccaa0827fc73
Carmy45 <carmi.fortunato@studenti.unina.it>
2026-07-14 22:44:25 +0200

    Riduzione code smells 1.1

--- src/main/java/dao/RichiestaDAO.java
+++ src/main/java/dao/RichiestaDAO.java
@@ -1,7 +1,6 @@
 package dao;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
--- src/main/java/implementazionedao/RichiestaPostgresDao.java
+++ src/main/java/implementazionedao/RichiestaPostgresDao.java
@@ -8,7 +8,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
